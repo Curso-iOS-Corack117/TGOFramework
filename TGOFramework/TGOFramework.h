a096ceb21ac5495fe266db51502d4e2bb131c38b
Ordaz Romero Sergio <orcheko@gmail.com>
2023-03-21 10:03:22 -0600

    Initial Commit

---
+++ TGOFramework/TGOFramework.h
@@ -0,0 +1,18 @@
+//
+//  TGOFramework.h
+//  TGOFramework
+//
+//  Created by Sergio Ordaz Romero on 21/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TGOFramework.
+FOUNDATION_EXPORT double TGOFrameworkVersionNumber;
+
+//! Project version string for TGOFramework.
+FOUNDATION_EXPORT const unsigned char TGOFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TGOFramework/PublicHeader.h>
+
+
